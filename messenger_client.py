f5a4f406325271b93eba7c5463608b15929fc05e
Axel <axel.andree@gmx.de>
2020-06-16 14:07:17 +0200

    added message_input class

---
+++ messenger_client.py
@@ -0,0 +1,28 @@
+
+#### message struct: @name message
+
+
+
+class message_input:
+    def __init__(self, content):
+        self.content = content
+        self.addresse = 0
+        if "@" in self.content:
+            self.at = self.content.find("@")
+            self.space = self.content.find(" ",self.at)
+            self.addresse = self.content[self.at+1: self.space]
+            self.message = self.content[self.space+1:len(self.content)]
+        else:
+            self.message = self.content
+
+a = message_input("helloworld")
+
+print(a.message)
+print(a.addresse)
+
+b = message_input("@s hello world")
+
+print(b.message)
+print(b.addresse)
+
+
