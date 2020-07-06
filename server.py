2667f904fa83e46221a7db15aa1d94559f44bfb9
Axel <axel.andree@gmx.de>
2020-07-06 13:25:39 +0200

    Added primitive multithreading

--- server.py
+++ server.py
@@ -1,4 +1,16 @@
 import socket
+import threading
+import time
+
+
+
+def Debug_Thread(connection):
+        connection.send(b"Debug_Thread")
+
+        print(connection.recv(1024))
+
+        time.sleep(100)
+        connection.close()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Create a socket object
 
@@ -9,11 +21,15 @@ port = 697
 s.bind(("192.168.178.31",port))
 
 s.listen(5)
+connections = []
+threads = []
 
 while True:
         c,addr = s.accept() #Establish a connection with the client
+        connections.append(c)
         print("Got connection from", addr)
 
-        c.send(b"Thank you for connecting!")
+        threads.append(threading.Thread(target = Debug_Thread, args = (connections[-1],)))
+        threads[-1].start()
         
-        c.close()
+        
